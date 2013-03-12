4f8096e94429d7a1c596fd8704b2893d8e0808e2
iLBear <2blissfrog@gmail.com>
2013-03-12 12:55:46 +0900

    Initial Commit

---
+++ TSND121sample_forOpenGL/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TSND121sample_forOpenGL
+//
+//  Created by H.Sakuma on 2013/03/12.
+//  Copyright (c) 2013年 H.Sakuma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
